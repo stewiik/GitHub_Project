31988b21107bebdd2694628c989cb56cff586a29
Wiktoria <w.stec19@gmail.com>
2024-07-28 15:42:31 +0200

    refactor names in method

--- src/main/java/com/github_project/controller/GithubRestController.java
+++ src/main/java/com/github_project/controller/GithubRestController.java
@@ -4,10 +4,7 @@ import com.github_project.dto.RepoWithBranchesResponseDto;
 import com.github_project.service.GithubService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class GithubRestController {
 
     @GetMapping("/repositories/{username}")
     public ResponseEntity<List<RepoWithBranchesResponseDto>> getReposWithBranches(@PathVariable String username,
-                                                                            @RequestParam(HttpHeaders.ACCEPT) String header) {
+                                                                                  @RequestHeader(HttpHeaders.ACCEPT) String header) {
 
         return ResponseEntity.ok(githubService.getRepos(username));
     }
