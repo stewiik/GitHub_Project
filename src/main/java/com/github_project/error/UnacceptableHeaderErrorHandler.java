371404dfb9e19a681eb9a3fce4f569588a73671e
Wiktoria <w.stec19@gmail.com>
2024-07-28 17:59:30 +0200

    add wrong header error handler

--- src/main/java/com/github_project/error/UnacceptableHeaderErrorHandler.java
+++ src/main/java/com/github_project/error/UnacceptableHeaderErrorHandler.java
@@ -3,21 +3,24 @@ package com.github_project.error;
 import com.github_project.controller.GithubRestController;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice(assignableTypes = GithubRestController.class)
+@ControllerAdvice
 @Log4j2
 public class UnacceptableHeaderErrorHandler {
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public ErrorResponseDto handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException exception) {
+    public ResponseEntity<ErrorResponseDto> handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException exception) {
         log.warn("HttpMediaTypeNotAcceptableException error while accessing repositories");
-        return new ErrorResponseDto(HttpStatus.NOT_ACCEPTABLE.value(), exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorResponseDto(HttpStatus.NOT_ACCEPTABLE.value(), "Unsupported Accept header"));
     }
 }
