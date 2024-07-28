44123c9b29c44b80cd707954abc26a7d75ea2898
Wiktoria <w.stec19@gmail.com>
2024-07-28 14:13:48 +0200

    first commit

---
+++ src/main/java/com/github_project/GitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubProjectApplication.class, args);
+    }
+
+}
