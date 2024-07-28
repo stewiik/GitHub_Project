7be77481e69d6d90af662a11578ab580decc5621
Wiktoria <w.stec19@gmail.com>
2024-07-28 15:49:16 +0200

    add warn log

--- src/main/java/com/github_project/error/UnacceptableHeaderErrorHandler.java
+++ src/main/java/com/github_project/error/UnacceptableHeaderErrorHandler.java
@@ -1,6 +1,7 @@
 package com.github_project.error;
 
 import com.github_project.controller.GithubRestController;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice(assignableTypes = GithubRestController.class)
+@Log4j2
 public class UnacceptableHeaderErrorHandler {
 
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ErrorResponseDto handleHttpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException exception) {
+        log.warn("HttpMediaTypeNotAcceptableException error while accessing repositories");
         return new ErrorResponseDto(HttpStatus.NOT_ACCEPTABLE.value(), exception.getMessage());
     }
 }
