b640b069f8c8841e2e5b66e28193338cb3c574d8
Wiktoria <w.stec19@gmail.com>
2024-07-28 18:03:36 +0200

    update controller to use unacceptable header error

--- src/main/java/com/github_project/controller/GithubRestController.java
+++ src/main/java/com/github_project/controller/GithubRestController.java
@@ -1,26 +1,41 @@
 package com.github_project.controller;
 
 import com.github_project.dto.RepoWithBranchesResponseDto;
+import com.github_project.error.UnacceptableHeaderErrorHandler;
 import com.github_project.service.GithubService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Log4j2
 public class GithubRestController {
     GithubService githubService;
 
-    public GithubRestController(GithubService githubService) {
+    UnacceptableHeaderErrorHandler handler;
+
+    public GithubRestController(GithubService githubService, UnacceptableHeaderErrorHandler handler) {
         this.githubService = githubService;
+        this.handler = handler;
     }
 
-    @GetMapping("/repositories/{username}")
-    public ResponseEntity<List<RepoWithBranchesResponseDto>> getReposWithBranches(@PathVariable String username,
-                                                                                  @RequestHeader(HttpHeaders.ACCEPT) String header) {
+    @GetMapping(value = "/repositories/{username}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getReposWithBranches(@PathVariable String username,
+                                                  @RequestHeader(HttpHeaders.ACCEPT) String header) throws HttpMediaTypeNotAcceptableException {
 
-        return ResponseEntity.ok(githubService.getRepos(username));
+        if (!MediaType.APPLICATION_JSON_VALUE.equals(header)) {
+            log.error("Unsupported Accept header: " + header);
+            return new ResponseEntity<>(handler.handleHttpMediaTypeNotAcceptableException(new HttpMediaTypeNotAcceptableException(List.of(MediaType.APPLICATION_JSON))), HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        List<RepoWithBranchesResponseDto> repos = githubService.getRepos(username);
+        return ResponseEntity.ok(repos);
     }
-
 }
