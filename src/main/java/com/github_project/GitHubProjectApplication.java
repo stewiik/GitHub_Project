5b0c8f1e76645f795d33ed3ff83e1dcfaabbcd2f
Wiktoria <w.stec19@gmail.com>
2024-07-28 15:43:14 +0200

    add annotation

--- src/main/java/com/github_project/GitHubProjectApplication.java
+++ src/main/java/com/github_project/GitHubProjectApplication.java
@@ -2,8 +2,11 @@ package com.github_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GitHubProjectApplication {
 
     public static void main(String[] args) {
