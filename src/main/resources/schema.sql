17deadd4da888ca3ea7ca758f1333cd4639b0796
Wiktoria <w.stec19@gmail.com>
2024-08-12 18:37:38 +0200

    add properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE repo;
-
-CREATE TABLE IF NOT EXISTS repo
-(
-    id     BIGSERIAL    PRIMARY KEY,
-    owner  VARCHAR(255) NOT NULL,
-    name   VARCHAR(255) NOT NULL
-    );
+-- DROP TABLE repo;
+--
+-- CREATE TABLE IF NOT EXISTS repo
+-- (
+--     id     BIGSERIAL    PRIMARY KEY,
+--     owner  VARCHAR(255) NOT NULL,
+--     name   VARCHAR(255) NOT NULL
+--     );
