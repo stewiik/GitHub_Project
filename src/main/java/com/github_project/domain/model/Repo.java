770df8f9dae711141207624fd3f67f9c158460a9
Wiktoria <w.stec19@gmail.com>
2024-08-12 18:41:18 +0200

    add Repo class

--- src/main/java/com/github_project/domain/model/Repo.java
+++ src/main/java/com/github_project/domain/model/Repo.java
@@ -1,4 +1,35 @@
 package com.github_project.domain.model;
 
-public record Repo(String name, Owner owner, boolean fork) {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Setter
+@Table(name = "repo")
+public class Repo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    String owner;
+
+    @Column(nullable = false)
+    String name;
+
+    public Repo() {
+    }
+
+    public Repo(String owner, String name) {
+        this.owner = owner;
+        this.name = name;
+    }
+
+    public Repo(Long id, String owner, String name) {
+        this.id = id;
+        this.owner = owner;
+        this.name = name;
+    }
 }
